e2898b49c712745aa8d1637f85764a272b220c5c
Gajus Kuizinas <gk@anuary.com>
2015-12-28 18:06:00 +0200

    Using new jsdoc plugin.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-module.exports = require('./eslintrc.json');
+module.exports = {
+    plugins: [
+        'jsdoc'
+    ],
+    rules: {
+        'jsdoc/check-param-names': 1,
+        'jsdoc/check-tag-names': 1,
+        'jsdoc/check-types': 1,
+        'jsdoc/newline-after-description': [
+            1,
+            'always'
+        ],
+        'jsdoc/require-hyphen-before-description': 0,
+        'jsdoc/require-param': 1,
+        'jsdoc/require-param-description': 0,
+        'jsdoc/require-param-type': 1,
+        'jsdoc/require-returns-description': 0,
+        'jsdoc/require-returns-type': 1
+    }
+};
